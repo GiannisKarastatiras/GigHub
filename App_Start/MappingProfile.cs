f81eb45ef99067e58ce346b684f345a179a01b99
Giannis Karastatiras <gianniskarastatiras@gmail.com>
2019-01-07 15:00:23 +0200

    Add files via upload

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GigHub_BC5.Dtos;
+using GigHub_BC5.Models;
+
+namespace GigHub_BC5.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ApplicationUser, UserDto>();
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
+            });
+            IMapper mapper = config.CreateMapper();
+        }
+    }
+}
