4d809084ea643bb5f2acae62ff47c78df085ba8a
Giannis Karastatiras <gianniskarastatiras@gmail.com>
2019-01-10 14:37:07 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GigHub_BC5.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHub_BC5.Startup))]
+namespace GigHub_BC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
